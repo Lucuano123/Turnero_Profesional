8d64f62043c3264c600a9a1821485f05b6473297
unknown <luliquaranta6@gmail.com>
2025-07-04 15:40:24 -0300

    Rama: Reserva-de-Turnos

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,11 @@
-import express from "express";
-import config from './config.js';
-console.log(`Backend URL: ${config.backendUrl}`);
-console.log(`JWT Secret: ${config.jwtSecret}`);
+import express from 'express';
+import { custumerRouter } from './custumer/custumer.routes.js';
+import { appointmentRouter } from './appointment/appointment.routes.js';
 const app = express();
 app.use(express.json());
-app.listen(config.port, () => {
-    console.log(`Servidor corriendo en https://localhost:${config.port}`);
+app.use('/api/custumers', custumerRouter);
+app.use('/api/appointments', appointmentRouter);
+app.listen(3000, () => {
+    console.log('Server runnning on http://localhost:3000/');
 });
 //# sourceMappingURL=app.js.map
